a1caa79ca9e359a7c27e09dee1af58d7f0fb7bb4
Tabeg Zouhair <tabeg,zouhair@gmail.com>
2024-12-31 00:08:56 +0100

    committed

--- app/src/main/java/com/example/smartwatchdarkmodeapp/ApiService.kt
+++ app/src/main/java/com/example/smartwatchdarkmodeapp/ApiService.kt
@@ -1,8 +1,19 @@
 package com.example.smartwatchdarkmodeapp
 
+import android.telecom.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+
+data class AddResponse(
+    val status: Int,
+    val status_message: String
+)
 
 interface ApiService {
     @GET("WatchAPI/readAll.php")
     fun getWatch(): retrofit2.Call<List<SmartWatch>>
+
+    @POST("WatchAPI/create.php")
+    fun addWatch(@Body watch: SmartWatch): retrofit2.Call<AddResponse>
 }
